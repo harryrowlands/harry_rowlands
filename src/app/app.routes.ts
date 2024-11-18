5639f63c6a4e688b2e9eb1b2d70a118b2021572f
Harry Rowlands <rowlands.h02@gmail.com>
2024-11-18 23:23:20 +1100

    Day 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './_components/home/home.component';
+import { ProjectsComponent } from './_components/projects/projects.component';
+import { PageNotFoundComponent } from './_components/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent},
+    { path: 'projects', component: ProjectsComponent},
+    { path: '**', component: PageNotFoundComponent},
+];
